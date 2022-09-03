993f7e1e35a4dc3f98db4db48dcb366abe0aa0f5
SWebDev55 <112745821+SWebDev55@users.noreply.github.com>
2022-09-03 16:02:01 +0700

    Update script.js

--- script.js
+++ script.js
@@ -5,3 +5,35 @@ function openNav() {
 function closeNav() {
     document.getElementById("myNav").style.width = "0%";
 }
+$(".page-scroll").click(function(){
+    $(".menu").hide();
+});
+
+// paralax scroll
+$(function() {
+    var $item = $('nav ul li');
+    
+    $item.on('click', 'a', function(event) {
+        var $section = $($(this).attr('href'));
+        var sectionTop = $section.offset().top;   
+    
+        $('html, body').stop().animate({scrollTop: sectionTop}, 800);
+        
+        event.preventDefault();
+    });
+    
+    $(window).scroll(function() {
+        var scrollTop = $(this).scrollTop();
+        
+        $item.each(function() {
+            var $section = $($(this).find('a').attr('href'));
+            var sectionTop = $section.offset().top - 60;
+            var sectionHeight = $section.height();
+            
+            if (sectionTop <= scrollTop && (sectionTop + sectionHeight) > scrollTop) {
+            $(this).addClass('active');
+            $(this).siblings().removeClass('active');
+            }
+        });
+    });
+});
